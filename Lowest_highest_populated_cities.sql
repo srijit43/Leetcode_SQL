d01e6e58160b41cbbadb474f1718e6392d20d2e4
Srijit Bhattacharya <56132120+srijit43@users.noreply.github.com>
2024-08-19 23:36:46 -0700

    Add files via upload
    EXL analytics lowest and highest populated cities in a state

---
+++ Lowest_highest_populated_cities.sql
@@ -0,0 +1,37 @@
+-- Display state, lowest and highest populated city
+/*CREATE TABLE city_population (
+    state VARCHAR(50),
+    city VARCHAR(50),
+    population INT
+);
+
+-- Insert the data
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'ambala', 100);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'panipat', 200);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'gurgaon', 300);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'amritsar', 150);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'ludhiana', 400);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'jalandhar', 250);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'mumbai', 1000);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'pune', 600);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'nagpur', 300);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'bangalore', 900);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mysore', 400);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mangalore', 200);
+*/
+
+-- Select * from city_population
+
+-- Solution
+
+With CTE as(
+Select *,
+Max(population) over (partition by state) as max_pop,
+Min(population) over (partition by state) as min_pop
+from city_population)
+
+Select state,
+max(case when population = max_pop then city end) as highest_populated,
+min(case when population = min_pop then city end) as lowest_populated
+from CTE
+group by state
